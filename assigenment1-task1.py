00f6fb726ce69a1245f561e662bcb17dd0d7c61b
Suresh Kumar <129381421+Sureshbhutta@users.noreply.github.com>
2025-07-27 10:18:14 +0530

    Add files via upload

---
+++ assigenment1-task1.py
@@ -0,0 +1,8 @@
+
+data1 = int(input('Enter the First Value: '))
+data2 = int(input('Enter the second Value: '))
+
+print('Addition: ',data1 + data2)
+print('Subtraction: ',data1 - data2)
+print('Multiplication: ',data1 * data2)
+print('Division: ',data1 / data2)
---
+++ assigenment1-task2.py
@@ -0,0 +1,5 @@
+
+data1 = input('Enter the First Name: ')
+data2 = input('Enter the Last Name: ')
+
+print('Hello,', data1 , data2 ,'Welcome to the Python Program.')
